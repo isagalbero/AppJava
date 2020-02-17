86ea6dc4db0a1775bb605b77205b6769efa40d5b
Isabella Galbero <isagalbero@gmail.com>
2020-02-17 10:10:49 -0300

    Importação java.util.Date para utilizar datas

--- primeiroprograma/PrimeiroPrograma.java
+++ primeiroprograma/PrimeiroPrograma.java
@@ -1,4 +1,5 @@
 package primeiroprograma;
+import java.util.Date;
 
 /**
  * PrimeiroPrograma
@@ -7,6 +8,7 @@ public class PrimeiroPrograma {
 
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
+        Date data = new Date();
     }
 
 }
